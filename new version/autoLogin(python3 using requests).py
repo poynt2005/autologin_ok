638f1d21c606c1d2773db162cb074cb9924f50a0
紀克勤 <poynt2010@hotmail.com>
2018-07-26 01:51:15 +0800

    add new version
    Use python3
    use requests library
    delete mechanize

---
+++ new version/autoLogin(python3 using requests).py
@@ -0,0 +1,128 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+
+
+class autoLogin:
+    def __init__(self , email , pwd):
+        self.email = email
+        self.pwd = pwd
+
+        self.session = requests.session()
+        self.expireDay = ''
+        self.currentMoney = 0
+        self.state = ''
+        
+    def initSession(self):
+
+        loginPage = self.session
+        
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+            }
+        
+        try:
+            loginPage = loginPage.get('http://okav.com.tw/do.php?act=login' , headers=headers)
+            return loginPage.text
+        except:
+            raise Exception('Connect Failed')
+
+    def login(self):
+
+        loginPage = self.initSession()
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+            }
+        data = {
+            'url' : 'http://okav.com.tw/',
+            'user_email' : self.email,
+            'user_passwd' : self.pwd
+            }
+        logger = self.session
+
+        try:
+            logger = logger.post('http://okav.com.tw/do.php?act=login' , headers=headers , data=data)
+
+            soup = BeautifulSoup(logger.text , 'html.parser')
+
+            if soup.find('p' , {'class':'lost'}):
+               raise Exception('ID incorrect')
+            return
+        except:
+            raise Exception('Connect Failed')
+
+    def sign(self):
+
+        dashboardPage = self.session
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+            }
+
+        def sendSignReq():
+            signer = self.session
+
+            try:
+                signer = signer.post('http://okav.com.tw/modules.php' , data={'day':'up'} , headers=headers)
+
+                if re.search('skin/okav_tw/images/A00[1-2].jpg', signer.text).group(0).split('/')[-1] == 'A002.jpg':
+                    return signer.text
+                else:
+                    return False
+
+            except:
+                raise Exception('Connect Failed')
+                
+        try:
+            dashboardPage = dashboardPage.get('http://okav.com.tw/modules.php' , headers=headers).text
+
+
+            signImg = re.search('skin/okav_tw/images/A00[1-2].jpg', dashboardPage).group(0).split('/')[-1]
+
+            isSigned = False
+            
+            if signImg == 'A001.jpg':
+                afterSinged = sendSignReq()
+                if afterSinged:
+                    dashboardPage = afterSinged
+                    isSigned = True
+                    self.state = 'Signed Success'
+            elif signImg == 'A002.jpg':
+                isSigned = True
+                self.state = 'Already Signed'
+
+            if isSigned:
+                soup = BeautifulSoup(dashboardPage , 'html.parser')
+                topInfo = soup.find('div' , {'class' : 'topInfo'})
+                normal = topInfo.find('p' , {'class' : 'normal'})
+
+                self.currentMoney = re.search('[0-9]+' , normal.find('font').contents[0]).group(0)
+                self.expireDay = re.search('[0-9]+\-[0-9]+\-[0-9]+' , normal.find('font').contents[1].text).group(0)
+                return
+            else:
+                self.state = 'Error happened'
+                return "Error happened"
+        except:
+            raise Exception('Connect Failed')
+        
+    def getMessage(self):
+        return {
+            'usr' : self.email,
+            'state' : self.state,
+            'current fund' : self.currentMoney,
+            'Expire Day' : self.expireDay
+            }
+        
+        
+            
+def main():
+    t = autoLogin("test003@gmail.com" , "test003")
+    t.login()
+    t.sign()
+    print(t.getMessage())
+
+if __name__ == '__main__':
+    main()
+    
---
+++ new version/mainLogger(python3 write2Log).py
@@ -0,0 +1,41 @@
+from autoLogin import autoLogin
+import json
+import datetime
+
+def writeToLog(currentId , signMessage ,error = False):
+    with open('log.txt' , 'a') as f:
+        if not error:
+            f.write('Logged ID : %s , currentTime : %s , %s\n' % (currentId , datetime.datetime.now() , signMessage))
+        else:
+            f.write('Logged ID : %s , currentTime : %s , Signed Failed ,maybe connection error or id incorrent\n' % (currentId , datetime.datetime.now()))
+    return
+def writeToJson(messageArr):
+    with open('currentState.json' , 'w') as f:
+        j = json.dumps(messageArr , ensure_ascii=False)
+        f.write(j)
+    return
+
+def main():
+    res = []
+
+    for i in range(0 , 3, 1):
+        email = 'poynt201%d@hotmail.com' % (i)
+        password = 'poynt201%d' % (i)
+
+        try:
+            a = autoLogin(email=email , pwd=password)
+            a.login()
+            a.sign()
+            mes = a.getMessage()
+            
+            writeToLog(email , mes['state'])
+            res.append(mes)
+        except:
+            writeToLog(email ,'' ,  True)
+            
+    if res:
+        writeToJson(res)
+
+
+if __name__ == '__main__':
+    main()
